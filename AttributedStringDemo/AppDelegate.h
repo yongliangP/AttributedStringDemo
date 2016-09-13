df2a59e40580d020e65eded83b9e2ba9b5e9f6da
yongliangPeng <1994577317@qq.com>
2016-09-13 16:31:32 +0800

    Initial Commit

---
+++ AttributedStringDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AttributedStringDemo
+//
+//  Created by 彭永亮 on 16/9/13.
+//  Copyright © 2016年 yongliangP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AttributedStringDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AttributedStringDemo
+//
+//  Created by 彭永亮 on 16/9/13.
+//  Copyright © 2016年 yongliangP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
